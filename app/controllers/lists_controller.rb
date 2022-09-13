6f743084627725386513b1ed1f07c90e3f99a684
EC2 Default User <ec2-user@ip-172-31-32-163.ap-northeast-1.compute.internal>
2022-09-13 13:53:05 +0000

    finish 9

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,14 +1,6 @@
 class ListsController < ApplicationController
   def new
-      # Viewへ渡すためのインスタンス変数に空のModelオブジェクトを生成する。
     @list = List.new
-  end
-
-  def create
-    list = List.new(list_params)
-    list.save
-
-    redirect_to list_path(list.id)
   end
 
   def index
@@ -27,6 +19,15 @@ class ListsController < ApplicationController
     list = List.find(params[:id])
     list.update(list_params)
     redirect_to list_path(list.id)
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list.id)
+    else
+      render :new
+    end
   end
 
   def destroy
